5cd56926dde8a1fad8f548c7e63a89ba28eaabb1
David Tegeltija <david.tegeltija@gmail.com>
2021-01-16 21:32:55 +0100

    Dodani linki na različne dele spletne strani

--- ucenje.py
+++ ucenje.py
@@ -1,15 +1,40 @@
 import bottle
 
 @bottle.get("/")
-def pozdravi():
-    return bottle.template("osnovna_stran.html")
+def basic():
+    return bottle.template("index.html")
+
+@bottle.get("/index")
+def index():
+    return bottle.template("index.html")
+
+@bottle.get("/recepti")
+def recepti():
+    return bottle.template("recepti.html")
+
+@bottle.get("/vseckano")
+def vseckano():
+    return bottle.template("vseckano.html")
+
+@bottle.get("/dodaj")
+def dodaj():
+    return bottle.template("dodaj.html")
 
 
-@bottle.post("/sestej/")
-def sestej():
-    a = int(bottle.request.forms["a"])
-    b = int(bottle.request.forms["b"])
-    return "{} + {} = {}".format(a, b, a + b)
+#@bottle.post("/isci/<vneseno>")
+#def isci(vneseno):
+#    return bottle.template
+
+#@bottle.get("/")
+#def staticna_spletna(style):
+#    pot = "C:/Users/David/Documents/Šola FAKS - 2.letnik/Spletna stran"
+#    return bottle.static_file(index.html, root=pot)
+
+#@bottle.post("/sestej/")
+#def sestej():
+#    a = int(bottle.request.forms["a"])
+#    b = int(bottle.request.forms["b"])
+#    return "{} + {} = {}".format(a, b, a + b)
 
 
 bottle.run(debug=True, reloader=True)
