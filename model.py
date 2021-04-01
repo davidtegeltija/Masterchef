481edde4ddc402ea55a63a37dfb2739d55d46339
David Tegeltija <david.tegeltija@gmail.com>
2021-04-01 22:05:56 +0200

    popravljeno dodajanje enakih slik

--- model.py
+++ model.py
@@ -3,12 +3,15 @@ import os
 import json
 import re
 
+
 path_to_json = os.path.join(os.getcwd(),"./", "Database", "data.json")
+
 
 def read_json():
     with open(path_to_json, "r", encoding="utf8") as json_file:
         data = json.load(json_file)
         return data
+
 
 def write_json(data):
     with open(path_to_json, "w", encoding="utf8") as json_file:
@@ -45,9 +48,9 @@ def seznam_podatkov():
                 seznam_za_osebo.append(data["recepti"][oseba][lastnosti])
             if lastnosti == "comments":
                 seznam_za_osebo.append(data["recepti"][oseba][lastnosti])
-
         seznam_podatkov.append(seznam_za_osebo)
     return seznam_podatkov
+
 
 def seznam_receptov():
     data = read_json()
@@ -56,13 +59,29 @@ def seznam_receptov():
         ime_recepta = data["recepti"][oseba]["title"]
         recepti.append(ime_recepta)
     return recepti
+
+
+def seznam_slik():
+    data = read_json()
+    slike = []
+    for oseba in data["recepti"]:
+        slika = data["recepti"][oseba]["image"]
+        if "$" in slika:
+            ime_slike = slika.split("/")[1]
+            ime_slike.split("$")
+            slike.append(ime_slike.split("$")[0] + ime_slike.split("$")[2])
+        else:
+            slike.append(slika.split("/")[1])
+    return slike
         
+
 def glasuj_za(recept):
     data = read_json()
     for oseba in data["recepti"]:
         if data["recepti"][oseba]["title"] == recept:
             data["recepti"][oseba]["likes"] += 1
     write_json(data)
+
 
 def glasuj_proti(recept):
     data = read_json()
@@ -71,10 +90,12 @@ def glasuj_proti(recept):
             data["recepti"][oseba]["dislikes"] += 1
     write_json(data)
 
+
 def preveri_slovnico(beseda):
     if re.match("^[A-Za-z0-9_-]*$", beseda):
         return True
     return False
+
 
 def prosto_uporabnisko_ime(ime, geslo):
     if not(preveri_slovnico(ime) and preveri_slovnico(geslo)):
@@ -85,6 +106,7 @@ def prosto_uporabnisko_ime(ime, geslo):
             return False
     return True
 
+
 def preveri_prijavo(ime, geslo):
     if preveri_slovnico(ime) and preveri_slovnico(geslo):
         data = read_json()
@@ -93,10 +115,12 @@ def preveri_prijavo(ime, geslo):
                 return True
     return False
 
+
 def zahtevaj_prijavo():
     if bottle.request.get_cookie("uporabnik"):
         return True
     return False
+
 
 def vsec_mi_je_recept(recept, uporabnisko_ime):
     data = read_json()
@@ -110,6 +134,7 @@ def vsec_mi_je_recept(recept, uporabnisko_ime):
                 data["uporabniki"][uporabnik]["liked_recipe"] = moji_recepti
     write_json(data)
 
+
 def ni_mi_vsec_recept(recept, uporabnisko_ime):
     data = read_json()
     for uporabnik in data["uporabniki"]:
@@ -121,6 +146,7 @@ def ni_mi_vsec_recept(recept, uporabnisko_ime):
                 moji_recepti.append(str(recept))
                 data["uporabniki"][uporabnik]["disliked_recipe"] = moji_recepti
     write_json(data)
+
 
 def vseckani_naslovi_receptov(uporabnisko_ime):
     data = read_json()
@@ -134,6 +160,7 @@ def vseckani_naslovi_receptov(uporabnisko_ime):
                 return []
     return vseckani
 
+
 def nevseckani_naslovi_receptov(uporabnisko_ime):
     data = read_json()
     vseckani = []
@@ -146,6 +173,7 @@ def nevseckani_naslovi_receptov(uporabnisko_ime):
                 return []
     return vseckani
 
+
 def vseckani_recepti(uporabnisko_ime):
     data = read_json()
     vseckani = []
@@ -157,9 +185,121 @@ def vseckani_recepti(uporabnisko_ime):
                         vseckani.append(recept[:5])
     return vseckani
 
+
 def moji_recepti(uporabnisko_ime):
     moji = []
     for recept in seznam_podatkov():
         if recept[0] == uporabnisko_ime:
             moji.append(recept)
     return moji
+
+
+def odstrani_recept(recept, uporabnik):
+    data = read_json()
+    vseckani_naslovi = vseckani_naslovi_receptov(uporabnik)
+    moji_recepti = vseckani_recepti(uporabnik)
+    for recepti in moji_recepti:
+        if recept == recepti[1]:
+            moji_recepti.remove(recepti)
+            vseckani_naslovi.remove(recepti[1])
+            for oseba in data["uporabniki"]:
+                if data["uporabniki"][oseba]["username"] == uporabnik:
+                    data["uporabniki"][oseba]["liked_recipe"] = vseckani_naslovi
+                write_json(data)  
+
+
+def domaca_stran(uporabnik):
+    data = read_json()
+    skupni_seznam_osebe = []
+    for oseba in data["uporabniki"]:
+        if data["uporabniki"][oseba]["username"] == uporabnik:
+            vsec_mi_je = data["uporabniki"][oseba]["liked_recipe"]
+            ni_mi_vsec = data["uporabniki"][oseba]["disliked_recipe"] 
+            skupni_seznam_osebe.append(vsec_mi_je)
+            skupni_seznam_osebe.append(ni_mi_vsec)
+    return skupni_seznam_osebe
+
+
+def komentiraj(recept, komentar):
+    data = read_json()
+    for recept_osebe in data["recepti"]:
+        if data["recepti"][recept_osebe]["title"] == recept:
+            data["recepti"][recept_osebe]["comments"].append(komentar)
+    write_json(data)
+
+
+def dodaj_recept(ime, naslov, sestavine, postopek, slika):
+    data = read_json()
+    ostevilcenje = len(data["recepti"]) + 1
+
+    if slika is not None:
+        if slika.filename not in seznam_slik():
+            file_path = "Database/{file}".format(file=slika.filename) 
+            slika.save(file_path)
+            data["recepti"][f"recept_{ostevilcenje}"] = {"owner" : ime, 
+                                                        "title" : naslov, 
+                                                        "ingredients" : sestavine, 
+                                                        "procedure" : postopek, 
+                                                        "image" : file_path, 
+                                                        "likes" : 0, 
+                                                        "dislikes" : 0, 
+                                                        "comments" : []}
+        else:
+            print(seznam_slik())
+            print(slika.filename)
+            ostevilcenje_slik = seznam_slik().count(slika.filename) + 1
+            novo_ime = slika.filename.split(".")
+            slika.filename = str(novo_ime[0] + f"${ostevilcenje_slik}$" + "." + novo_ime[1])
+            print(slika.filename)
+            file_path = "Database/{file}".format(file=slika.filename) 
+            slika.save(file_path)  
+            data["recepti"][f"recept_{ostevilcenje}"] = {"owner" : ime, 
+                                                        "title" : naslov, 
+                                                        "ingredients" : sestavine, 
+                                                        "procedure" : postopek, 
+                                                        "image" : file_path, 
+                                                        "likes" : 0, 
+                                                        "dislikes" : 0, 
+                                                        "comments" : []}                         
+        write_json(data)
+    else:
+        data["recepti"][f"recept_{ostevilcenje}"] = {"owner" : ime, 
+                                                    "title" : naslov, 
+                                                    "ingredients" : sestavine, 
+                                                    "procedure" : postopek, 
+                                                    "image" : "Database/noImage.png", 
+                                                    "likes" : 0, 
+                                                    "dislikes" : 0, 
+                                                    "comments" : []}
+        write_json(data)
+
+
+def uredi(recept):
+    data = read_json()
+    for recept_osebe in data["recepti"]:
+        if data["recepti"][recept_osebe]["title"] == recept:
+            sestavine = data["recepti"][recept_osebe]["ingredients"]
+    return sestavine
+
+
+def shrani(Recept, nove_sestavine, nov_postopek, nova_slika):
+    data = read_json()
+    for recept in data["recepti"]:
+        if data["recepti"][recept]["title"] == Recept:
+            if nove_sestavine is not None:
+                data["recepti"][recept]["ingredients"] = nove_sestavine
+            if nov_postopek is not None:
+                data["recepti"][recept]["procedure"] = nov_postopek
+            if nova_slika is not None:
+                if nova_slika.filename not in seznam_slik():
+                    file_path = "Database/{file}".format(file=nova_slika.filename) 
+                    nova_slika.save(file_path)
+                    data["recepti"][recept]["image"] = file_path
+                else:
+                    ostevilcenje = seznam_slik().count(nova_slika.filename) + 1
+                    novo_ime = nova_slika.filename.split(".")
+                    nova_slika.filename = str(novo_ime[0] + f"${ostevilcenje}$" + "." + novo_ime[1])
+                    file_path = "Database/{file}".format(file=nova_slika.filename) 
+                    nova_slika.save(file_path)
+                    data["recepti"][recept]["image"] = file_path
+    write_json(data)
--- website.py
+++ website.py
@@ -3,6 +3,7 @@ import os
 import json
 from model import *
 
+
 @bottle.route("/static/<filename>")
 def staticen_server(filename):
     return bottle.static_file(filename, root= "./Database")
@@ -12,25 +13,29 @@ def staticen_server(filename):
 def index(): 
     podatki = seznam_podatkov() 
     if zahtevaj_prijavo():
-        data = read_json()
-        for oseba in data["uporabniki"]:
-            if data["uporabniki"][oseba]["username"] == bottle.request.get_cookie("uporabnik"):
-                vsec_mi_je = data["uporabniki"][oseba]["liked_recipe"]
-                ni_mi_vsec = data["uporabniki"][oseba]["disliked_recipe"]                
-        return bottle.template("index.tpl", podatki = podatki, vseckani_recepti = vsec_mi_je, nevseckani_recepti = ni_mi_vsec, uporabnik = bottle.request.get_cookie("uporabnik"))
-    return bottle.template("index.tpl", podatki = podatki, uporabnik = "Gost")
+        uporabnik = bottle.request.get_cookie("uporabnik")
+        njegovi_recepti = domaca_stran(uporabnik)            
+        return bottle.template("index.tpl", 
+                                podatki = podatki, 
+                                vseckani_recepti = njegovi_recepti[0], 
+                                nevseckani_recepti = njegovi_recepti[1], 
+                                uporabnik = bottle.request.get_cookie("uporabnik")
+                                )
+    return bottle.template("index.tpl", 
+                            podatki = podatki, 
+                            uporabnik = "Gost")
+
 
 @bottle.post("/komentiraj/<recept>")
-def komentiraj(recept):
+def Komentiraj(recept):
     if zahtevaj_prijavo():
         komentar = bottle.request.forms.get("Komentar")
-        data = read_json()
-        for recept_osebe in data["recepti"]:
-            if data["recepti"][recept_osebe]["title"] == recept:
-                data["recepti"][recept_osebe]["comments"].append(komentar)
-        write_json(data)
+        komentiraj(recept, komentar)
         return bottle.redirect("/")
-    return bottle.template("neveljavna_prijava.tpl", sporocilo = "Za komentiranje receptov se morate prijaviti na spletno stran", uporabnik = "Gost")
+    return bottle.template("neveljavna_prijava.tpl", 
+                            sporocilo = "Za komentiranje receptov se morate prijaviti na spletno stran", 
+                            uporabnik = "Gost")
+
 
 @bottle.post("/glasuj_za")
 def Glasuj_za():
@@ -42,7 +47,9 @@ def Glasuj_za():
                 vsec_mi_je_recept(recept, bottle.request.get_cookie("uporabnik"))           
         return bottle.redirect("/")
     else:
-        return bottle.template("neveljavna_prijava.tpl", sporocilo = "Za všečkanje receptov se morate prijaviti na spletno stran", uporabnik = "Gost")
+        return bottle.template("neveljavna_prijava.tpl", 
+                                sporocilo = "Za všečkanje receptov se morate prijaviti na spletno stran", 
+                                uporabnik = "Gost")
                     
 
 @bottle.post("/glasuj_proti")
@@ -55,7 +62,9 @@ def Glasuj_proti():
                 ni_mi_vsec_recept(recept, bottle.request.get_cookie("uporabnik"))
         return bottle.redirect("/")
     else:
-        return bottle.template("neveljavna_prijava.tpl", sporocilo = "Za ne všečkanje receptov se morate prijaviti na spletno stran", uporabnik = "Gost")
+        return bottle.template("neveljavna_prijava.tpl", 
+                                sporocilo = "Za ne všečkanje receptov se morate prijaviti na spletno stran", 
+                                uporabnik = "Gost")
 
 
 @bottle.get("/vseckano")
@@ -63,93 +72,89 @@ def vseckano():
     if zahtevaj_prijavo():
         vseckani_naslovi = vseckani_naslovi_receptov(bottle.request.get_cookie("uporabnik"))
         moji_recepti = vseckani_recepti(bottle.request.get_cookie("uporabnik"))
-        return bottle.template("vseckano.tpl", uporabnik = bottle.request.get_cookie("uporabnik"), vseckani_recepti = moji_recepti, vseckane_slike = vseckani_naslovi)
-    return bottle.template("neveljavna_prijava.tpl", sporocilo = "Če želite videti recepte, ki ste jih všečkali se prijavite.", uporabnik = "Gost")
+        return bottle.template("vseckano.tpl", 
+                                uporabnik = bottle.request.get_cookie("uporabnik"), 
+                                vseckani_recepti = moji_recepti, 
+                                vseckane_slike = vseckani_naslovi)
+    return bottle.template("neveljavna_prijava.tpl", 
+                            sporocilo = "Če želite videti recepte, ki ste jih všečkali se prijavite.", 
+                            uporabnik = "Gost")
+
 
 @bottle.post("/odstrani")
 def odstrani():
-    data = read_json()
-    vseckani_naslovi = vseckani_naslovi_receptov(bottle.request.get_cookie("uporabnik"))
-    moji_recepti = vseckani_recepti(bottle.request.get_cookie("uporabnik"))
-    for recept in moji_recepti:
-        if bottle.request.POST.get("odstrani") == recept[1]:
-            moji_recepti.remove(recept)
-            vseckani_naslovi.remove(recept[1])
-            for oseba in data["uporabniki"]:
-                if data["uporabniki"][oseba]["username"] == bottle.request.get_cookie("uporabnik"):
-                    data["uporabniki"][oseba]["liked_recipe"] = vseckani_naslovi
-                write_json(data)    
-    return bottle.template("vseckano.tpl", uporabnik = bottle.request.get_cookie("uporabnik"), vseckani_recepti = moji_recepti, vseckane_slike = vseckani_naslovi) 
-
+    uporabnik = bottle.request.get_cookie("uporabnik")
+    recept = bottle.request.POST.get("odstrani")
+    odstrani_recept(recept, uporabnik)
+    vseckani_naslovi = vseckani_naslovi_receptov(uporabnik)
+    moji_recepti = vseckani_recepti(uporabnik)
+    return bottle.template("vseckano.tpl", 
+                            uporabnik = bottle.request.get_cookie("uporabnik"), 
+                            vseckani_recepti = moji_recepti, 
+                            vseckane_slike = vseckani_naslovi) 
 
 
 @bottle.get("/dodaj")
 def dodaj():
     if zahtevaj_prijavo():
         return bottle.template("dodaj.tpl", uporabnik = bottle.request.get_cookie("uporabnik"))
-    return bottle.template("neveljavna_prijava.tpl", sporocilo = "Za dodajanje receptov se morate prijaviti na spletno stran", uporabnik = "Gost")
+    return bottle.template("neveljavna_prijava.tpl", 
+                            sporocilo = "Za dodajanje receptov se morate prijaviti na spletno stran", 
+                            uporabnik = "Gost")
+
 
 @bottle.post("/dodaj_recept")
-def dodaj_recept():
+def Dodaj_recept():
     ime = bottle.request.get_cookie("uporabnik")
     naslov = bottle.request.forms.get("Naslov")
     sestavine = bottle.request.forms.get("Sestavine")
     postopek = bottle.request.forms.get("Postopek")
     slika = bottle.request.files.get("Slika")
-
-    data = read_json()
-    index = len(data["recepti"]) + 1
-
+    
     if slika is not None:
         name, ext = os.path.splitext(slika.filename)
         if ext not in (".png", ".jpg", ".jpeg"):
             return bottle.template("napacna_datoteka.tpl", uporabnik = bottle.request.get_cookie("uporabnik"))
-        file_path = "Database/{file}".format(file=slika.filename) 
-        slika.save(file_path)
-        data["recepti"][f"recept_{index}"] = {"owner" : ime, "title" : naslov, "ingredients" : sestavine, "procedure" : postopek, "image" : file_path, "likes" : 0, "dislikes" : 0, "comments" : []}
-        write_json(data)
-    else:
-        data["recepti"][f"recept_{index}"] = {"owner" : ime, "title" : naslov, "ingredients" : sestavine, "procedure" : postopek, "image" : "Database/noImage.png", "likes" : 0, "dislikes" : 0, "comments" : []}
-        write_json(data)
+    dodaj_recept(ime, naslov, sestavine, postopek, slika)
     return bottle.redirect("/")
         
+
 @bottle.get("/vasi_recepti")
 def vasi_recepti():
     recepti = moji_recepti(bottle.request.get_cookie("uporabnik"))
-    return bottle.template("vasi_recepti.tpl", vasi_recepti = recepti, uporabnik = bottle.request.get_cookie("uporabnik"))
+    return bottle.template("vasi_recepti.tpl", 
+                            vasi_recepti = recepti, 
+                            uporabnik = bottle.request.get_cookie("uporabnik")
+                            )
+
 
 @bottle.get("/uredi/<recept>")
-def uredi(recept):
-    data = read_json()
+def Uredi(recept):
     for celoten_recept in seznam_podatkov():
         if celoten_recept[1] == recept: 
             urejani_recept = celoten_recept
             bottle.response.set_cookie("recept", urejani_recept[1], path='/')
-            for recept_osebe in data["recepti"]:
-                if data["recepti"][recept_osebe]["title"] == recept:
-                    sestavine = data["recepti"][recept_osebe]["ingredients"]
-    return bottle.template("uredi.tpl", recept = urejani_recept, uporabnik = bottle.request.get_cookie("uporabnik"), sestavine = sestavine)
+            sestavine = uredi(recept)
+    return bottle.template("uredi.tpl", 
+                            recept = urejani_recept, 
+                            sestavine = sestavine,
+                            uporabnik = bottle.request.get_cookie("uporabnik")
+                            )
+
 
 @bottle.post("/shrani")
-def shrani():
+def Shrani():
     nove_sestavine = bottle.request.forms.get("Nove_sestavine")
     nov_postopek = bottle.request.forms.get("Nov_postopek")
     nova_slika = bottle.request.files.get("Nova_slika")
-    data = read_json()
-    for recept in data["recepti"]:
-        if data["recepti"][recept]["title"] == bottle.request.get_cookie("recept"):
-            if nove_sestavine is not None:
-                data["recepti"][recept]["ingredients"] = nove_sestavine
-            if nov_postopek is not None:
-                data["recepti"][recept]["procedure"] = nov_postopek
-            if nova_slika is not None:
-                name, ext = os.path.splitext(nova_slika.filename)
-                if ext not in (".png", ".jpg", ".jpeg"):
-                    return bottle.template("napacna_datoteka.tpl", uporabnik = bottle.request.get_cookie("uporabnik"))
-                file_path = "Database/{file}".format(file=nova_slika.filename) 
-                nova_slika.save(file_path)
-                data["recepti"][recept]["image"] = file_path
-    write_json(data)
+    recept = bottle.request.get_cookie("recept")
+    if nova_slika is not None:
+        name, ext = os.path.splitext(nova_slika.filename)
+        if ext not in (".png", ".jpg", ".jpeg"):
+            return bottle.template("napacna_datoteka.tpl", 
+                                    uporabnik = bottle.request.get_cookie("uporabnik")
+                                    )
+    shrani(recept, nove_sestavine, nov_postopek, nova_slika)
     return bottle.redirect("/vasi_recepti")
 
 
@@ -157,10 +162,12 @@ def shrani():
 def prijava():
     return bottle.template("prijava.tpl", uporabnik = "Gost")
 
+
 @bottle.get("/odjava")
 def odjava():
     bottle.response.delete_cookie("uporabnik")
     return bottle.redirect("/")
+
 
 @bottle.get("/nov_profil")
 def nov_profil():
@@ -168,10 +175,11 @@ def nov_profil():
         return bottle.template("registracija.tpl", uporabnik = bottle.request.get_cookie("uporabnik"))
     return bottle.template("registracija.tpl", uporabnik = "Gost")
 
+
 @bottle.post("/logiraj_me")
 def logiraj_me():
     data = read_json()
-    index = len(data["uporabniki"]) + 1
+    ostevilcenje = len(data["uporabniki"]) + 1
 
     if bottle.request.forms.get("Uporabnisko_ime") and bottle.request.forms.get("Geslo"):
         uporabnisko_ime = bottle.request.forms.get("Uporabnisko_ime")
@@ -180,24 +188,35 @@ def logiraj_me():
             bottle.response.set_cookie("uporabnik", uporabnisko_ime, path='/')
             return bottle.redirect('/')
         else:
-            return bottle.template("neveljavna_prijava.tpl", sporocilo = "Vaše uporabniško ime ali geslo je napačno", uporabnik = "Gost")
-    
+            return bottle.template("neveljavna_prijava.tpl", 
+                                    sporocilo = "Vaše uporabniško ime ali geslo je napačno", 
+                                    uporabnik = "Gost")
+
     if bottle.request.forms.get("Novo_uporabnisko_ime") and bottle.request.forms.get("Novo_geslo"):  
         nov_uporabnik = bottle.request.forms.get("Novo_uporabnisko_ime")
         novo_geslo = bottle.request.forms.get("Novo_geslo")
         if prosto_uporabnisko_ime(nov_uporabnik, novo_geslo):
             bottle.response.set_cookie("uporabnik", nov_uporabnik, path='/')
-            data["uporabniki"][f"person_{index}"] = {"username" : nov_uporabnik, "password" : novo_geslo, "liked_recipe" : [], "disliked_recipe" : []}
+            data["uporabniki"][f"person_{ostevilcenje}"] = {"username" : nov_uporabnik, 
+                                                            "password" : novo_geslo, 
+                                                            "liked_recipe" : [], 
+                                                            "disliked_recipe" : []}
             write_json(data)
             return bottle.redirect('/')
         else:
-            return bottle.template("neveljavna_prijava.tpl", sporocilo = "Uporabniško ime je že zasedeno")
+            return bottle.template("neveljavna_prijava.tpl", 
+                                    sporocilo = "Uporabniško ime je že zasedeno",
+                                    uporabnik = "Gost")
     else:
-        return bottle.template("neveljavna_prijava.tpl", sporocilo = "Uporabniško ime in geslo nemoreta biti prazna", uporabnik = "Gost")
+        return bottle.template("neveljavna_prijava.tpl", 
+                                sporocilo = "Uporabniško ime in geslo nemoreta biti prazna", 
+                                uporabnik = "Gost")
+
 
 @bottle.get("/spremenite_geslo")
 def sprememba_gesla():
     return bottle.template("sprememba_gesla.tpl", uporabnik = bottle.request.get_cookie("uporabnik"))
+
 
 @bottle.post("/spremenite_geslo")
 def sprememba_gesla():
@@ -212,7 +231,10 @@ def sprememba_gesla():
         write_json(data)
         return bottle.redirect("/")
     else:
-        return bottle.template("neveljavna_prijava.tpl", sporocilo = "Staro geslo ni pravilno", uporabnik = bottle.request.get_cookie("uporabnik"))
+        return bottle.template("neveljavna_prijava.tpl", 
+                                sporocilo = "Staro geslo ni pravilno", 
+                                uporabnik = bottle.request.get_cookie("uporabnik")
+                                )
 
 
 #bottle.request.query se uporablja za GET poizvedbe
